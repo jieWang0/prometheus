d207486ffefe204b044433caac2e73cc2a204deb
jieWang <jie.wang@transwarp.io>
2018-03-01 14:10:48 +0800

    perfect code

--- src/main/java/io/jprometheus/interceptor/PrometheusActiveRequestInterceptor.java
+++ src/main/java/io/jprometheus/interceptor/PrometheusActiveRequestInterceptor.java
@@ -12,18 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 public class PrometheusActiveRequestInterceptor extends HandlerInterceptorAdapter {
 
     static final Gauge activeRequests = Gauge.build()
-            .name("Active_Request_Total").labelNames("path", "method", "status")
+            .name("Active_Request_Total").labelNames("path", "method")
             .help("Active Requests").register();
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        activeRequests.labels(request.getServletPath(), request.getMethod(), String.valueOf(response.getStatus())).inc();
+        activeRequests.labels(request.getServletPath(), request.getMethod()).inc();
         return super.preHandle(request, response, handler);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        activeRequests.labels(request.getServletPath(), request.getMethod(), String.valueOf(response.getStatus())).dec();
+        activeRequests.labels(request.getServletPath(), request.getMethod()).dec();
+
+        if(activeRequests.labels(request.getServletPath(), request.getMethod()).get() == 0) {
+            activeRequests.remove(request.getServletPath(), request.getMethod());
+        }
         super.afterCompletion(request, response, handler, ex);
     }
 }
